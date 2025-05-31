ca93cde5df42b850bee1fef0458784b86f80b1a9
Mårten Åsberg <marten.asberg@outlook.com>
2025-05-31 11:36:19 +0200

    Make `PhoneNumber` equatable

--- Domain/Primitives/PhoneNumber.cs
+++ Domain/Primitives/PhoneNumber.cs
@@ -6,7 +6,7 @@ using System.Text.RegularExpressions;
 namespace Split.Domain.Primitives;
 
 [DebuggerDisplay("{Value}")]
-public partial class PhoneNumber
+public partial class PhoneNumber : IEquatable<PhoneNumber>
 {
     [GeneratedRegex(@"^\+?\d+$")]
     private static partial Regex PhoneNumberValidator { get; }
@@ -25,4 +25,19 @@ public partial class PhoneNumber
 
     [ExcludeFromCodeCoverage]
     public override string ToString() => Value;
+
+    public bool Equals(PhoneNumber? other) => other is not null && Value.Equals(other.Value);
+
+    [ExcludeFromCodeCoverage]
+    public override bool Equals(object? obj) => Equals(obj as PhoneNumber);
+
+    [ExcludeFromCodeCoverage]
+    public override int GetHashCode() => Value.GetHashCode();
+
+    [ExcludeFromCodeCoverage]
+    public static bool operator ==(PhoneNumber? left, PhoneNumber? right) =>
+        left is null ? right is null : left.Equals(right);
+
+    [ExcludeFromCodeCoverage]
+    public static bool operator !=(PhoneNumber? left, PhoneNumber? right) => !(left == right);
 }
